d0e4acc609a29084fd1c56e23c47d3c489aa7989
João Heitor <jhmartinsrdo@gmail.com>
2023-03-23 20:52:52 -0300

    Revert "chore: adding chat and more components"
    This reverts commit a349041831f3fbc0f209bcf771f9458077c0144b.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import Sidebar from "../components/Sidebar";
-import Chat from "../components/Chat";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <div className="container">
-        <Sidebar />
-        <Chat />
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
