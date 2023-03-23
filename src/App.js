85638b4e8f63524a3346c8d18b82800e25be38b2
João Heitor <jhmartinsrdo@gmail.com>
2023-03-23 20:55:31 -0300

    Revert "build: firebase and react-router-dom dependency"
    This reverts commit 6df0d03aa74fef36a8b06442dd60c16ed490b43e.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,9 @@ import "./style.scss";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const { currentUser } = useContext(AuthContext);
-  console.log(currentUser);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,62 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Add from "../img/addAvatar.png";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, db, storage } from "../firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore";
-import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const [err, setErr] = useState(false);
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const displayName = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const file = e.target[3].files[0];
-
-    try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
-      const storageRef = ref(storage, displayName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        (error) => {
-          setErr(true);
-        },
-        async () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateProfile(res.user, {
-              displayName,
-              photoURL: downloadURL,
-            });
-
-            await setDoc(doc(db, "users", res.user.uid), {
-              uid: res.user.uid,
-              displayName,
-              email,
-              photoURL: downloadURL,
-            });
-
-            await setDoc(doc(db, "userChats", res.user.uid), {});
-            navigate("/");
-          });
-        }
-      );
-    } catch (err) {
-      setErr(true);
-    }
-  };
-
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Chat Livre</span>
         <span className="title">Cadastro</span>
-        <form onSubmit={handleSubmit}>
+        <form>
           <input type="text" placeholder="nome" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="senha" />
@@ -66,7 +17,6 @@ const Register = () => {
             <span>Adicione um avatar</span>
           </label>
           <button>Cadastrar</button>
-          {err && <span>Algo deu errado</span>}
         </form>
         <p>Já possue uma conta? Login</p>
       </div>
