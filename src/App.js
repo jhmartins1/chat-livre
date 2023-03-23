6df0d03aa74fef36a8b06442dd60c16ed490b43e
João Heitor <jhmartinsrdo@gmail.com>
2023-03-23 20:52:16 -0300

    build: firebase and react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import "./style.scss";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  return <Home />;
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
