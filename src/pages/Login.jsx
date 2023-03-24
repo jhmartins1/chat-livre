d76edba13151cfed6ae968480e62996bd7a1682d
João Heitor <jhmartinsrdo@gmail.com>
2023-03-23 21:22:26 -0300

    feat: Login auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,40 @@
-import React from "react";
-import Add from "../img/addAvatar.png";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Chat Livre</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="senha" />
           <button>Entrar</button>
+          {err && <span>Algo deu errado</span>}
         </form>
-        <p>Não possue uma conta? Registre</p>
+        <p>
+          Não possue uma conta? <Link to="/register">Registre</Link>
+        </p>
       </div>
     </div>
   );
