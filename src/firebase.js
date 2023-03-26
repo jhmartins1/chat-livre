e07c0c8cd3247c125be5779371be02ae7d53292d
João Heitor <jhmartinsrdo@gmail.com>
2023-03-26 19:58:50 -0300

    refactor: change to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2FM5dnTwuAxkYdRfJWimRrifg5RaV6mA",
-  authDomain: "chat-livre.firebaseapp.com",
-  projectId: "chat-livre",
-  storageBucket: "chat-livre.appspot.com",
-  messagingSenderId: "133283429179",
-  appId: "1:133283429179:web:46b73a5cb30c76cd63951e",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
